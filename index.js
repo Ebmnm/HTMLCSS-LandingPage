acddd93219bb6e77d57adc057a96ba62221af7e0
ebmnm <ebmnm@aol.com>
2021-05-20 13:47:26 -0400

    animated

---
+++ index.js
@@ -0,0 +1,19 @@
+
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.from(`.hero`, {
+    opacity:0.1, duration:1, y:-50
+})
+
+gsap.from(`.boot-sec`, {
+    opacity:.3, 
+    duration:1,
+    x:400
+})
+
+gsap.from(`.full-banner`, {
+    duration:1,
+    scrollTrigger:`.full-banner`,
+    opacity:.3,
+    y:200
+})
